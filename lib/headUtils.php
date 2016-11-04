330395499a8602d1fcd548b7cf90f7783354a6e1
chenoajai <winsteadchenoa@gmail.com>
2016-11-04 09:35:43 -0600

    added Folders
    added lib, images, partials etc. added links etc

---
+++ lib/headUtils.php
@@ -0,0 +1,18 @@
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+
+  <meta charset="utf-8">
+
+
+  <link rel="stylesheet" href="lib/css/bootstrap.css">
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
+  rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
+  crossorigin="anonymous">
+
+
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="lib/css/style.css">
---
+++ lib/partials/navBar.php
@@ -0,0 +1,6 @@
+
+<nav class="navbar navbar-default navbar-static-top">
+  <div class="container">
+    ...
+  </div>
+</nav>
