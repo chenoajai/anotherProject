10c8639784c3180d62123846ca705f48c4de1f33
chenoajai <winsteadchenoa@gmail.com>
2016-11-04 11:30:19 -0600

    added divs. added images

--- index.php
+++ index.php
@@ -9,5 +9,65 @@
 <body>
   <?php require_once("lib/partials/navBar.php");?>
 
+  <div class="container" id="container">
+    <h1 id="headText">The Site</h1>
+    <p>
+      Welcome to the bomb.com
+    </p>
+    <i class="fa fa-facebook" id="facebook" aria-hidden="true"></i>
+    <i class="fa fa-envelope" id="mail" aria-hidden="true"></i>
+    <i class="fa fa-twitter" id="twitter" aria-hidden="true"></i>
+  </div>
+
+<br>
+
+  <div class="container" id="container1">
+    <h1>Enter Email</h1>
+    <form class="form-inline">
+  <div class="form-group">
+    <label for="exampleInputEmail2">Email</label>
+    <input type="email" class="form-control"  id="example" placeholder="jane.doe@example.com"><i class="fa fa-envelope" aria-hidden="true"></i>
+  </div>
+</form>
+  </div>
+
+  <br>
+
+  <div class="container">
+    <div class="row">
+      <h1>Our Team</h1>
+      <div class="col-md-4">
+          <img class="img-responsive img-circle" src="lib/images/n1.jpg" alt="" />
+      </div>
+      <div class="col-md-4">
+          <img class="img-responsive img-circle" src="lib/images/k1.jpg" alt="" />
+      </div>
+      <div class="col-md-4">
+          <img class="img-responsive img-circle" src="lib/images/p1.jpg" alt="" />
+      </div>
+
+    </div>
+
+  </div>
+  <br>
+  <br>
+  <br>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2">
+      
+
+      </div>
+
+    </div>
+
+
+  </div>
+
+<script type="text/javascript" src="lb/js/bootstrap.js">
+
+
+</script>
   </body>
 </html>
